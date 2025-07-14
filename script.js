c0fda7a77fce5c942ee764485608ce81e05f9eed
Shubham277353 <golubhandari405@gmail.com>
2025-07-14 19:21:28 +0530

    improved the css

--- script.js
+++
@@ -1,6 +0,0 @@
-let menuButton  = document.getElementById("menuButton")
-let menu = document.getElementById("menu")
-
-menuButton.addEventListener("click",function(){
-    menu.classList.toggle('active');
-})
